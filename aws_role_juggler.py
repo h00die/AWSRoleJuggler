473a95823ea30be1f74ddd242aec09a4777b304a
h00die <h00die@users.noreply.github.com>
2024-08-14 18:17:25 +0000

    black

--- aws_role_juggler.py
+++ aws_role_juggler.py
@@ -10,35 +10,35 @@ ACCESS_ID = "AWS_ACCESS_KEY_ID"
 SECRET_KEY = "AWS_SECRET_ACCESS_KEY"
 SESSION_KEY = "AWS_SESSION_TOKEN"
 
+
 def assumeRole(client, arn):
-    response = client.assume_role (
-        RoleArn = arn,
-        RoleSessionName = 'juggle',
-        DurationSeconds = 3600
-    )
+    response = client.assume_role(RoleArn=arn, RoleSessionName="juggle", DurationSeconds=3600)
 
     if not response:
         print("[!] Failed to assume role. Exiting.")
         return
 
-    if 'Credentials' not in response:
+    if "Credentials" not in response:
         print("[!] No credentials returned. Exiting.")
         return
-    
-    credentials = response['Credentials']
+
+    credentials = response["Credentials"]
     print(f"[*] Expiration: {credentials['Expiration']}")
 
     print(f"{credentials['AccessKeyId']}\n{credentials['SecretAccessKey']}\n{credentials['SessionToken']}\n")
 
-    session = boto3.session.Session(aws_access_key_id=credentials['AccessKeyId'],
-                                    aws_secret_access_key = credentials['SecretAccessKey'],
-                                    aws_session_token=credentials['SessionToken'])
+    session = boto3.session.Session(
+        aws_access_key_id=credentials["AccessKeyId"],
+        aws_secret_access_key=credentials["SecretAccessKey"],
+        aws_session_token=credentials["SessionToken"],
+    )
 
-    client= session.client('sts')
+    client = session.client("sts")
     return client
 
+
 def juggleRoles(roleList):
-    client = boto3.client('sts') 
+    client = boto3.client("sts")
 
     first_role = roleList.pop(0)
     roleList.append(first_role)
@@ -47,19 +47,19 @@ def juggleRoles(roleList):
     # Do the first role assumption
 
     try:
-        while(True):
+        while True:
             print("[*] Sleeping for 15 minutes and then refreshing session.")
             time.sleep(540)
             for role in roleList:
                 client = assumeRole(client, role)
-    
+
     except KeyboardInterrupt:
         return
- 
+
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument('-r', '--role-list', nargs='+', default=[])
+    parser.add_argument("-r", "--role-list", nargs="+", default=[])
     args = parser.parse_args()
 
     juggleRoles(args.role_list)
--- find_circular_trust.py
+++ find_circular_trust.py
@@ -4,8 +4,9 @@ import boto3
 import json
 import networkx as nx
 
+
 def iam_list_roles():
-    iam_client = boto3.client('iam')
+    iam_client = boto3.client("iam")
     roles = []
     response = {}
     is_truncated = False
@@ -15,15 +16,16 @@ def iam_list_roles():
             if is_truncated is False:
                 response = iam_client.list_roles()
             else:
-                response = iam_client.list_roles(Marker=response['Marker'])
+                response = iam_client.list_roles(Marker=response["Marker"])
 
-            for role in response['Roles']:
+            for role in response["Roles"]:
                 roles.append(role)
 
-            is_truncated = response['IsTruncated']
+            is_truncated = response["IsTruncated"]
         return roles
     except Exception as e:
         raise
+
 
 def getCycles(aws_roles):
     g = nx.DiGraph()
@@ -31,10 +33,10 @@ def getCycles(aws_roles):
         g.add_nodes_from([role["Arn"]])
 
     for role in aws_roles:
-        for statement in role['AssumeRolePolicyDocument']['Statement']:
-            if statement['Effect'] == "Allow" and 'AWS' in statement['Principal']:
-                arns = statement['Principal']['AWS']
-                source = role['Arn']
+        for statement in role["AssumeRolePolicyDocument"]["Statement"]:
+            if statement["Effect"] == "Allow" and "AWS" in statement["Principal"]:
+                arns = statement["Principal"]["AWS"]
+                source = role["Arn"]
                 if isinstance(arns, list):
                     for arn in arns:
                         dest = arn
@@ -46,8 +48,9 @@ def getCycles(aws_roles):
     cycles = list(nx.simple_cycles(g))
     return cycles
 
+
 if __name__ == "__main__":
-    client = boto3.client('iam')
+    client = boto3.client("iam")
     aws_roles = iam_list_roles()
 
     cycles = getCycles(aws_roles)
